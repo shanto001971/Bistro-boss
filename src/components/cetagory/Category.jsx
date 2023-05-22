576c19a4dc2ef79b653382ce46dc7f5edcf4b37a
Shanto Islam <rajs45693@gmail.com>
2023-05-23 03:20:09 +0600

    add category section title

--- src/components/cetagory/Category.jsx
+++ src/components/cetagory/Category.jsx
@@ -8,13 +8,19 @@ import Slide2 from '../../assets/home/slide2.jpg';
 import Slide3 from '../../assets/home/slide3.jpg';
 import Slide4 from '../../assets/home/slide4.jpg';
 import Slide5 from '../../assets/home/slide5.jpg';
+import SectionTitle from "../sectionTitle/SectionTitle";
 
 
 
 
 const Category = () => {
     return (
-        <>
+        <section>
+            <SectionTitle
+                subHeading={"from 11.00 to 10.00 pm"}
+                heading={"Order Online"}
+            >
+            </SectionTitle>
             <Swiper
                 slidesPerView={3}
                 spaceBetween={30}
@@ -25,13 +31,28 @@ const Category = () => {
                 modules={[FreeMode, Pagination]}
                 className="mySwiper"
             >
-                <SwiperSlide><img src={Slide1} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={Slide2} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={Slide3} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={Slide4} alt="" /></SwiperSlide>
-                <SwiperSlide><img src={Slide5} alt="" /></SwiperSlide>
+                <SwiperSlide className="">
+                    <img src={Slide1} alt="" className="" />
+                    <h1 className="text-4xl font-thin uppercase ml-16 -mt-16 text-white mb-20">SHALAD</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Slide2} alt="" />
+                    <h1 className="text-4xl font-thin uppercase ml-16 -mt-16 text-white mb-20">soups</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Slide3} alt="" />
+                    <h1 className="text-4xl font-thin uppercase ml-16 -mt-16 text-white mb-20">pizzas</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Slide4} alt="" />
+                    <h1 className="text-4xl font-thin uppercase ml-16 -mt-16 text-white mb-20">dsserts</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={Slide5} alt="" />
+                    <h1 className="text-4xl font-thin uppercase ml-16 -mt-16 text-white mb-20">SHALAD</h1>
+                </SwiperSlide>
             </Swiper>
-        </>
+        </section>
     );
 };
 
